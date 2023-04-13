b4f84b06cf20ed3c8c46ea4d7ab1f56e3f1c7dce
Ruslan <r1554899@gmail.com>
2023-04-13 03:11:51 -0700

    search bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { useState, useRef, useEffect } from 'react';
 import Welcome from './components/Welcome';
 import Nav from './components/Nav';
+import Search from './components/Search';
+import './App.css';
 
 function App() {
   const user = {id: 1, username: "Husky_Joe", name: "joe", unreadMessages: ["hello", "hi", "how are you"], cart: ["socks", "shoes", "pants"], pfp: "https://openaicom.imgix.net/cd67d3b8-cd30-416c-b54b-b75d8cc2e728/morgan-stanley.jpeg?auto=compress%2Cformat&fit=min&fm=jpg&q=80&rect=079%2C0%2C4000%2C4000&w=1000"}
 
   return (
     <>
-    <Nav user={user} />
-    <Welcome user={user} />
-    
+        <Nav user={user} />
+        <Welcome user={user} />
+        <Search />
     </>
   )
 }
---
+++ src/components/Search.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Search.css'
+
+
+export default function Search() {
+  return (
+    <>
+    <div className='search-wrapper'>
+        <input name="search" className='search' placeholder='Search' />
+    </div>
+        
+    </>
+  )
+}
