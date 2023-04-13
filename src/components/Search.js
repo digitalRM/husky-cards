01508ba44daf595625a827bcbfa83d93368415c3
Ruslan <70782025+digitalRM@users.noreply.github.com>
2023-04-13 04:19:46 -0700

    font size fix

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import './Search.css'
-
+import FeatherIcon from 'feather-icons-react';
 
 export default function Search() {
+
+  function handleSubmit(e) {
+    e.target.search.value = '';
+    e.preventDefault();
+  }
+
   return (
     <>
     <div className='search-wrapper'>
+      <form onSubmit={handleSubmit}>
         <input name="search" className='search' placeholder='Search' />
-    </div>
-        
+          <button className='search-btn' type="submit">
+            <FeatherIcon icon="search" className="search-icon" color="#FFF" />
+          </button>
+        </form>
+      </div> 
     </>
   )
 }
