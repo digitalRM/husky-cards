bfa966968a2bff0caf26768f8d55097b59c023f4
Ruslan <r1554899@gmail.com>
2023-04-13 01:04:20 -0700

    combination of husky cards

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { useState, useRef, useEffect } from 'react';
-// import TodoList from './TodoList';
-// import { v4 as uuidv4 } from 'uuid';
-import './global.css';
-import Welcome from './Welcome';
+import Welcome from './components/Welcome';
+import Nav from './components/Nav';
 
 function App() {
   const user = {id: 1, username: "Husky_Joe", name: "joe", unreadMessages: ["hello", "hi", "how are you"], cart: ["socks", "shoes", "pants"]}
 
   return (
     <>
+    <Nav />
     <Welcome user={user} />
+    
     </>
   )
 }
