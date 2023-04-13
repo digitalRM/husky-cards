e2335a65437cf501ded3cfb2a245b88d7bebf2d4
Ruslan <70782025+digitalRM@users.noreply.github.com>
2023-04-13 15:54:44 -0700

    recommended section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Welcome from './components/Welcome';
 import Nav from './components/Nav';
 import Search from './components/Search';
 import './App.css';
+import Recommended from './components/Recommended';
 
 function App() {
   const user = {id: 1, username: "Husky_Joe", name: "joe", unreadMessages: ["hello", "hi", "how are you"], cart: ["socks", "shoes", "pants"], pfp: "https://openaicom.imgix.net/cd67d3b8-cd30-416c-b54b-b75d8cc2e728/morgan-stanley.jpeg?auto=compress%2Cformat&fit=min&fm=jpg&q=80&rect=079%2C0%2C4000%2C4000&w=1000"}
@@ -12,6 +13,7 @@ function App() {
         <Nav user={user} />
         <Welcome user={user} />
         <Search />
+        <Recommended />
     </>
   )
 }
---
+++ src/components/Recommended.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Recommended.css'
+
+export default function Recommended() {
+  return (
+    <>
+    <div className='recommended-wrapper'>
+
+        <div className='recommended'>
+
+        </div>
+        <div className='recommend-color-cover'></div>
+    </div>
+    
+
+    </>
+  )
+}
