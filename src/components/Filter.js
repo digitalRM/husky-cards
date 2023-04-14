640bce1657cdf3f6975a5ddd35f8bf6b0d26de68
Ruslan <70782025+digitalRM@users.noreply.github.com>
2023-04-14 06:25:55 -0700

    filter dropdown changes

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -7,6 +7,48 @@ import './Filter.css'
 import Popper from '@mui/material/Popper';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Divider } from '@mui/material';
+import Fade from '@mui/material/Fade';
+const theme = createTheme({
+  components: {
+    // Name of the component
+    MuiList: {
+      styleOverrides: {
+        root: {
+          // marginRight: '18px',
+        },
+      },
+    },
+
+    MuiButtonBase: {
+      styleOverrides: {
+        root: {
+          paddingRight: '180px',
+        },
+      },
+    },
+
+    MuiMenuItem: {
+      styleOverrides: {
+        root: {
+          fontFamily: 'Inter',
+          paddingRight: '32px',
+          fontWeight: '400',
+          transition: 'all 0.2s ease-in-out',
+          ':hover': {
+            backgroundColor: '#2E2E2E',
+          },
+        },
+      },
+    },
+    MuiButtonBase: {
+      defaultProps: {
+        disableRipple: true, // No more ripple, on the whole application 💣!
+      },
+    },
+  },
+});
 
 export default function PositionedMenu() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -18,8 +60,10 @@ export default function PositionedMenu() {
     setAnchorEl(null);
   };
 
+
   return (
     <div>
+      <ThemeProvider theme={theme}>
       <Button
         className="Button"
         disableRipple={true}
@@ -41,6 +85,8 @@ export default function PositionedMenu() {
       </Button>
 
       <Menu
+        theme={theme}
+        TransitionComponent={Fade}
         className="filter-menu"
         anchorEl={anchorEl}
         open={open}
@@ -53,28 +99,35 @@ export default function PositionedMenu() {
           style: {
             // left: '50%',
             transition: 'none',
-            borderRadius: '10px',
-            //transform: 'translateX(10%) translateY(45%)',
+            borderRadius: '15px',
+            //transform: 'translateX(5%) translateY(5%)',
             backgroundColor: '#1E1E1E',
             color: '#FFF',
           }
         }}
-
-        sx={{
-            
-        }}
       >
         <MenuItem sx={{fontFamily: 'Inter',}} onClick={handleClose}>
           <ListItemIcon>
-            <FeatherIcon icon="dollar-sign" color="#FFF" />
+            <FeatherIcon icon="dollar-sign" stroke-width="2" size="20px" color="#FFF" />
           </ListItemIcon>
-
-          <ListItemText sx={{fontFamily: 'Inter', fontWeight: 800}}>Price</ListItemText>
-          
+          By Price
         </MenuItem>
-        <MenuItem sx={{fontFamily: 'Inter',}} onClick={handleClose}>Quality</MenuItem>
-        <MenuItem sx={{fontFamily: 'Inter',}} VAonClick={handleClose}>Rarity</MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <FeatherIcon icon="check-circle" stroke-width="2" size="20px" color="#FFF" />
+          </ListItemIcon>
+          
+          By Quality
+          </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <FeatherIcon icon="award" stroke-width="2" size="20px" color="#FFF" />
+          </ListItemIcon>
+          
+          By Rarity
+          </MenuItem>
       </Menu>
+      </ThemeProvider>
     </div>
   );
 }
