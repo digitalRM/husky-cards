1d53cde62bdd931ae85e01c65154043051dff484
Ruslan <r1554899@gmail.com>
2023-04-14 03:57:33 -0700

    l

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -5,6 +5,8 @@ import MenuItem from '@mui/material/MenuItem';
 import FeatherIcon  from 'feather-icons-react';
 import './Filter.css'
 import Popper from '@mui/material/Popper';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 
 export default function PositionedMenu() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -45,18 +47,16 @@ export default function PositionedMenu() {
         onClose={handleClose}
         anchorOrigin={{
           vertical: 'top',
-          horizontal: 'left',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
+          horizontal: 'right',
         }}
         PaperProps={{
           style: {
             // left: '50%',
             transition: 'none',
-            transform: 'translateX(10%) translateY(45%)',
+            borderRadius: '10px',
+            //transform: 'translateX(10%) translateY(45%)',
             backgroundColor: '#1E1E1E',
+            color: '#FFF',
           }
         }}
 
@@ -64,9 +64,16 @@ export default function PositionedMenu() {
             
         }}
       >
-        <MenuItem sx={{fontFamily: 'Inter',}} onClick={handleClose}>Profile</MenuItem>
-        <MenuItem sx={{fontFamily: 'Inter',}} onClick={handleClose}>My account</MenuItem>
-        <MenuItem sx={{fontFamily: 'Inter',}} VAonClick={handleClose}>Logout</MenuItem>
+        <MenuItem sx={{fontFamily: 'Inter',}} onClick={handleClose}>
+          <ListItemIcon>
+            <FeatherIcon icon="dollar-sign" color="#FFF" />
+          </ListItemIcon>
+
+          <ListItemText sx={{fontFamily: 'Inter', fontWeight: 800}}>Price</ListItemText>
+          
+        </MenuItem>
+        <MenuItem sx={{fontFamily: 'Inter',}} onClick={handleClose}>Quality</MenuItem>
+        <MenuItem sx={{fontFamily: 'Inter',}} VAonClick={handleClose}>Rarity</MenuItem>
       </Menu>
     </div>
   );
